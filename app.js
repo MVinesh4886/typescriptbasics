3d02ab1d7315766840dcfa5769a9aaa94743b9f9
vinesh <vineshkrishna26@gmail.com>
2023-11-30 18:36:30 +0530

    infer types

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+var numberOne = document.getElementById("num1");
+var numberTwo = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function add(num1, num2) {
     return num1 + num2;
 }
-console.log(add(1, 5));
-console.log(add("1", "5"));
+buttonElement.addEventListener("click", function () {
+    var num1 = numberOne.value;
+    var num2 = numberTwo.value;
+    var result = add(+num1, +num2);
+    console.log(result);
+});
+// console.log(add(1, 5));
+// console.log(add("1", "5"));
--- app.ts
+++ app.ts
@@ -1,7 +1,18 @@
-function add(num1, num2) {
+let numberOne = document.getElementById("num1") as HTMLInputElement;
+let numberTwo = document.getElementById("num2") as HTMLInputElement;
+let buttonElement = document.querySelector("button");
+
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-console.log(add(1, 5));
+buttonElement.addEventListener("click", function () {
+  let num1 = numberOne.value;
+  let num2 = numberTwo.value;
+  let result = add(+num1, +num2);
+  console.log(result);
+});
 
-console.log(add("1", "5"));
+// console.log(add(1, 5));
+
+// console.log(add("1", "5"));
